b78a87cc9c40e1a0d6e824f21734f5daea395004
Павло Гончарук <pavlogoncaruk@MacBook-Pro-Pavlo.local>
2021-04-03 21:12:06 +0300

    Accelerometer settings4

--- AccDisplay/main/main.ino
+++ AccDisplay/main/main.ino
@@ -8,17 +8,23 @@ void setup()
 {
 Wire.begin();  
 Serial.begin(9600);
-
+Wire.write(0x6B);                 
+Wire.write(0x00);                  
+Wire.endTransmission(true); 
 }
 void loop()
 {
 Wire.beginTransmission(MPU);
 Wire.write(0x3B);
+Wire.endTransmission(false);
 Wire.requestFrom(MPU, 6, true);
-accx= Wire.read()<<8 | Wire.read();  
-accy = Wire.read()<<8 | Wire.read();  
-accz = Wire.read()<<8 | Wire.read();
+accx= (Wire.read()<<8 | Wire.read()) / 16384.0;  
+accy = (Wire.read()<<8 | Wire.read()) / 16384.0;  
+accz = (Wire.read()<<8 | Wire.read()) / 16384.0;
+Serial.print("X= ");
 Serial.println(accx);
+Serial.print("Y= ");
 Serial.println(accy);
+Serial.print("Z= ");
 Serial.println(accz);
 }
